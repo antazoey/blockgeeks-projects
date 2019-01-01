6598aea5af7f177dbf875c724ee4ca82a77fdbb5
Juliya <juliya@juliyasmith.com>
2019-01-01 13:37:06 -0600

    Update

--- src/counter.js
+++ src/counter.js
@@ -2,7 +2,7 @@ import Web3 from "web3"
 
 function initContract() {
     const url = "https://rinkeby.infura.io/v3/f0f5243a226d40e48cfec3e753ee640d";
-    const web3 = new Web3(new Web3.providers.HttpProvider(url));
+    const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider(url));
     let address = "0xd2d424cd70b687a6f2cedd0b1024399c8e8b2dd3";
     const abi = [
         {
@@ -45,6 +45,24 @@ function initContract() {
         }
     ];
     return new web3.eth.Contract(abi, address);
+}
+
+function printNetwork() {
+    web3.version.getNetwork((err, netId) => {
+        switch (netId) {
+            case "1":
+                console.log('This is mainnet');
+                break;
+            case "2":
+                console.log('This is the deprecated Morden test network.');
+                break;
+            case "3":
+                console.log('This is the ropsten test network.');
+                break;
+            default:
+                console.log(`This is an unknown network ${netId}.`);
+        }
+    });
 }
 
 export const contract = initContract();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
-import $ from 'jquery';
-import { contract, name } from "./counter.js";
+import { name } from "./counter.js";
+import * as Contract from "./call_counter.js";
 
 function componentTitle() {
     const element = document.createElement('div');
@@ -22,62 +22,6 @@ function componentButton(data) {
     return btn;
 }
 
-async function loadCurrentCount(valComp) {
-    const methods = contract.methods;
-    if (methods) {
-        const result = await methods.getCounter().call();
-        if (valComp) {
-            valComp.innerHTML = result;
-        } else {
-            console.log("Error with init contract");
-        }
-    }
-}
-
-function addCounter(i) {
-    let comp = $('#valcomp');
-    if (comp) {
-        let v = parseInt(comp.html(), 10);
-        comp.empty();
-        comp.html(v + i);
-    } else {
-        console.log("error with comp");
-        console.log(comp);
-    }
-}
-
-async function incrementCounter() {
-    const methods = contract.methods;
-    const acct = web3.eth.defaultAccount;
-    try {
-        const tx = await methods.increment().send( {from: acct} );
-        console.log(tx);
-        addCounter(1);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-async function decrementCounter() {
-    const methods = contract.methods;
-    const acct = web3.eth.defaultAccount;
-    try {
-        const tx = await methods.decrement().send( {from: acct} );
-        console.log(tx);
-        addCounter(1);
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-function reset() {
-    const comp = $('#valcomp');
-    if (comp) {
-        comp.empty();
-        comp.html(0);
-    }
-}
-
 function main() {
     console.log("Welcome friends");
     const title = componentTitle();
@@ -89,10 +33,10 @@ function main() {
     inc_btn.id = "increment";
     dec_btn.id = "decrement";
     reset_btn.id = "reset";
-    inc_btn.onclick = incrementCounter;
-    dec_btn.onclick = decrementCounter;
-    reset_btn.onclick = reset;
-    loadCurrentCount(valComp);
+    inc_btn.onclick = Contract.incrementCounter;
+    dec_btn.onclick = Contract.decrementCounter;
+    reset_btn.onclick = Contract.reset;
+    Contract.loadCurrentCount(valComp);
     document.body.appendChild(title);
     document.body.appendChild(valComp);
     document.body.appendChild(inc_btn);
